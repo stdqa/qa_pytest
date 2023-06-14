8faac1f057e55e61e7fbbd26d76440281422db58
ADi Adiko <adikaoew111@gmail.com>
2023-06-14 18:02:54 +0300

    Added test for login page, check url, register and login form is visible

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,30 @@
 from .page.main_page import MainPage
+from .page.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = 'http://selenium1py.pythonanywhere.com'
+    link = 'http://selenium1py.pythonanywhere.com/'
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    # login_page = LoginPage(browser, browser.current_url)
+    # login_page.should_be_login_page()
+
+def test_guest_should_see_login_link(browser):
+    link = 'http://selenium1py.pythonanywhere.com/'
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+# def test_url_in_login_page(browser):
+#     link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
+#     page = LoginPage(browser, link)
+#     page.open()
+#     page.should_be_login_url()
+
+def test_should_be_login_page(browser):
+    link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
+
