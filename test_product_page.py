bb469404196e5d4aa67aa85a3a09e857cb527cfc
ADi Adiko <adikaoew111@gmail.com>
2023-06-15 15:13:01 +0300

    new test with mark xfail and mark negative_cheks

--- test_product_page.py
+++ test_product_page.py
@@ -2,6 +2,21 @@ import pytest
 from .page.product_page import ProductPage
 
 
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+
 @pytest.mark.parametrize('promo_offer', [pytest.param(i, marks=pytest.mark.xfail(i=='7', reason='Server working not correctly')) for i in range(10)])
 def test_guest_can_add_product_to_basket(browser, promo_offer):
     #link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear'
