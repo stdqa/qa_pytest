b51ce92796b0dfdc3dfcff7fc14eaf06ea609695
ADi Adiko <adikaoew111@gmail.com>
2023-06-15 18:41:15 +0300

    Tests for review

--- test_main_page.py
+++ test_main_page.py
@@ -38,14 +38,3 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_not_be_items_to_buy()
     basket_page.should_be_message_basket_is_empty()
-
-
-@pytest.mark.start
-def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/'
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_basket()
-    basket_page = BasketPage(browser, browser.current_url)
-    basket_page.should_not_be_items_to_buy()
-    basket_page.should_be_message_basket_is_empty()
