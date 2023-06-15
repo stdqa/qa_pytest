ef4cb661dfd18757111c32368bb1a76bfa6c870b
ADi Adiko <adikaoew111@gmail.com>
2023-06-15 15:07:51 +0300

    new methods for go to login page, checking login link and go to basket

--- page/base_page.py
+++ page/base_page.py
@@ -1,7 +1,7 @@
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from .locators import BasePageLocators
 
 class BasePage():
     def __init__(self, browser, url, timeout=5):
@@ -42,3 +42,15 @@ class BasePage():
             return False
 
         return True
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK_INVALID)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
+    def go_to_basket(self):
+        link = self.browser.find_element(*BasePageLocators.BASKET_BUTTON)
+        link.click()
+
