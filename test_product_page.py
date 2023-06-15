6fb75f360e3871c46c7c5f93879a8d7bb243076a
ADi Adiko <adikaoew111@gmail.com>
2023-06-15 11:55:17 +0300

    Added 3 tests for negative checks

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,7 @@
 import pytest
 from .page.product_page import ProductPage
 
+
 @pytest.mark.parametrize('promo_offer', [pytest.param(i, marks=pytest.mark.xfail(i=='7', reason='Server working not correctly')) for i in range(10)])
 def test_guest_can_add_product_to_basket(browser, promo_offer):
     #link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear'
@@ -11,3 +12,31 @@ def test_guest_can_add_product_to_basket(browser, promo_offer):
     page.solve_quiz_and_get_code()
     page.should_be_confirm_message()
     page.should_be_same_price_in_basket()
+
+
+@pytest.mark.xfail
+@pytest.mark.negative_checks
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket_click()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.negative_checks
+def test_guest_cant_see_success_message(browser):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.xfail
+@pytest.mark.negative_checks
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket_click()
+    page.should_disappear_success_message()
