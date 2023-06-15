29f2b648cdb6d5290781dfcc1ca77edcb782181e
ADi Adiko <adikaoew111@gmail.com>
2023-06-15 15:06:10 +0300

    2 new tests for checking that when basket is empty. guest cant see products in basket opened from main page and product page

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,8 @@
 from .page.main_page import MainPage
 from .page.login_page import LoginPage
+from .page.basket_page import BasketPage
+from .page.product_page import ProductPage
+import pytest
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -18,11 +21,6 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
-# def test_url_in_login_page(browser):
-#     link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
-#     page = LoginPage(browser, link)
-#     page.open()
-#     page.should_be_login_url()
 
 def test_should_be_login_page(browser):
     link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
@@ -30,3 +28,24 @@ def test_should_be_login_page(browser):
     page.open()
     page.should_be_login_page()
 
+
+@pytest.mark.start
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = 'http://selenium1py.pythonanywhere.com'
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_not_be_items_to_buy()
+    basket_page.should_be_message_basket_is_empty()
+
+
+@pytest.mark.start
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_not_be_items_to_buy()
+    basket_page.should_be_message_basket_is_empty()
